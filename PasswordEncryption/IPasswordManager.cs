adaf37b046551ebb80736e3271e307bca9c442de
invisloa <91217120+invisloa@users.noreply.github.com>
2024-03-28 00:04:39 +0100

    Add project files.

---
+++ PasswordEncryption/IPasswordManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordEncryption
+{
+	public interface IPasswordManager
+	{
+		byte[] GenerateSalt(int size);
+		byte[] HashPasswordWithSalt(byte[] password, byte[] salt);
+		byte[] HashPasswordWithSalt(string password, byte[] salt);
+		bool VerifyPassword(byte[] password, byte[] salt, byte[] hash);
+	}
+}
---
+++ PasswordEncryption/PasswordManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordEncryption
+{
+	internal class PasswordManager
+	{
+		public static byte[] GenerateSalt(int size)
+		{
+			var rng = new RNGCryptoServiceProvider();
+			var salt = new byte[size];
+			rng.GetBytes(salt);
+			return salt;
+		}
+		public static byte[] HashPasswordWithSalt(byte[] password, byte[] salt)
+		{
+			using (var sha256 = SHA256.Create())
+			{
+				byte[] combined = password.Concat(salt).ToArray();
+				return sha256.ComputeHash(combined);
+			}
+		}
+		public static byte[] HashPasswordWithSalt(string password, byte[] salt)
+		{
+			return HashPasswordWithSalt(Encoding.UTF8.GetBytes(password), salt);
+		}
+		public static bool VerifyPassword(byte[] password, byte[] salt, byte[] hash)
+		{
+			return HashPasswordWithSalt(password, salt).SequenceEqual(hash);
+		}
+	}
+}
